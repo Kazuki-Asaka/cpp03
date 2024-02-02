be363eaa9a7ba785c0c5b25ae9ca4a80efdc36dc
Asaka Kazuki <akazuki@c1r23s11.42tokyo.jp>
2024-02-02 19:18:44 +0900

    re

---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,29 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() : ClapTrap(){
+	std::cout << "ScavTrap Default constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+	std::cout << "ScavTrap Name constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& rhs) {
+	std::cout << "ScavTrap Copy constructor called" << std::endl;
+	*this = rhs;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap Destructor called" << std::endl;
+}
+
+void ScavTrap::attack(const std::string target) {
+	if (this -> energy_point <= 0)
+		std::cout << this -> name << "is out of energy. " << this -> name << " has " << this -> energy_point << " points of energy" <<std::endl;
+	else if (this -> hit_point <= 0) 
+		std::cout << this -> name << "is dead. " << this -> name << " has " << this -> hit_point << " points of hit "<< std::endl;
+	else {
+		std::cout << "ClapTrap " << this -> name << " attacks " << target <<", causing " << this -> attack_damage << " points of damage!" << std::endl;
+		this -> energy_point = this -> energy_point - 1;
+	}
+}
